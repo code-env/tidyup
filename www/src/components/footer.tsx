02661f9f6177ad9b424396683b3ccaf4eb61042f
code-env <org.codeen@gmail.com>
2024-12-29 00:18:54 +0100

    feat: added the design inspiration on the footer

--- www/src/components/footer.tsx
+++ www/src/components/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="border-t py-8 mt-40 mx-auto max-w-xl w-full">
+    <footer className="border-t py-8 mt-40 mx-auto max-w-xl w-full flex items-center justify-between">
       <p className="flex gap-1 items-center">
         <img
           src="https://avatars.githubusercontent.com/u/135658967"
@@ -15,6 +15,17 @@ const Footer = () => {
           className="font-bold"
         >
           Bossadi Zenith
+        </a>
+      </p>
+      <p className="classnem">
+        Design inspired by{" "}
+        <a
+          href="https://sonner.emilkowal.ski/"
+          target="_blank"
+          rel="noreferrer"
+          className="font-bold"
+        >
+          Sonner
         </a>
       </p>
     </footer>
